f6f1a50f0f5506cabbf0b1e68da44f37b9ae9aca
oxweiwei <student@ZXW.local>
2016-06-01 10:36:28 +0800

    oxweiwei

---
+++ TheFirstTeam/TheFirstTeam/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  TheFirstTeam
+//
+//  Created by student on 16/6/1.
+//  Copyright © 2016年 ZXW. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
